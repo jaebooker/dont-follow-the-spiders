fee6bb52361db68488ba2e3e218f09434c920eb4
Andrija Zoric <andrija.zoric@yahoo.co.uk>
2019-03-20 19:41:01 -0700

    added friendly scene

---
+++ Assets/Lowpoly Toon Cat Lite/Demo/Cat_Lite_Demo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Cat_Lite_Demo : MonoBehaviour {
+
+	public Text camRotateCheck;
+	bool camRotate = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(camRotate)
+			Camera.main.transform.RotateAround (Vector3.zero, Vector3.up, 40 * Time.deltaTime);
+	}
+
+	public void CamRotate(){
+		if (camRotate) {
+			camRotate = false;
+			camRotateCheck.gameObject.SetActive (false);
+		} 
+		else {
+			camRotate = true;
+			camRotateCheck.gameObject.SetActive (true);
+		}
+
+	}
+}
