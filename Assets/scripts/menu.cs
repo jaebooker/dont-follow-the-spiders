fcf705d23ff7dd068d2d66b29a4af6dd691af4f7
Jaeson Booker <jaesonbooker@gmail.com>
2019-03-21 19:32:00 -0700

    added menu

---
+++ Assets/scripts/menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void battleMode()
+    {
+        SceneManager.LoadScene("battle");
+    }
+    public void endGame()
+    {
+        Application.Quit();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
