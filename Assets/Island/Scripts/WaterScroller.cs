921c9e5cc260a790e16989ab41dd906e8f3cf5e7
Jaeson Booker <jaesonbooker@gmail.com>
2019-03-22 16:21:41 -0700

    added safe scene with cats and a beach

---
+++ Assets/Island/Scripts/WaterScroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScroller : MonoBehaviour {
+	public float scrollSpeed = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+		if(GetComponent<Renderer>().material.shader.isSupported)
+			Camera.main.depthTextureMode |= DepthTextureMode.Depth;
+
+		float offset = Time.time * scrollSpeed;
+		//Texture scrolling is instanced separately | Best if your scene contains multiple water planes of different speeds
+		//GetComponent.<Renderer>().material.SetTextureOffset ("_MainTex", Vector2(offset/10.0, offset));
+
+		//Share texture scrolling among objects with the same material | Best if your scene contains a single water plane, or multiple water of the same speed
+		Vector2 k = new Vector2(offset / 10.0f, offset);
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", k);
+	}
+}
