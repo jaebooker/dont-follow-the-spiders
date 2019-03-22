e29d967b2ef1c2bc4f9e22923d368a62cba72635
Jaeson Booker <jaesonbooker@gmail.com>
2019-03-21 18:10:12 -0700

    added scripts and other scenes

---
+++ Assets/scripts/spiderCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spiderCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision spiderInfo)
+    {
+        if (spiderInfo.collider.tag == "weapon")
+        {
+            Debug.Log("They were singing, bye, bye, mr spider guy");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
