7f7d4c95151f5a4af6883b55d69885bacce7a7b6
Jaeson Booker <jaesonbooker@gmail.com>
2019-03-22 15:28:30 -0700

    finally added panic button

---
+++ Assets/scripts/panic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class panic : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetButtonDown("Fire1")) {
+			Debug.Log("asfegweg");
+			SceneManager.LoadScene("peace");
+		}
+		if (OVRInput.Get (OVRInput.Button.One)) {
+			Debug.Log("asfegweg");
+			SceneManager.LoadScene("peace");
+		}
+    }
+}
