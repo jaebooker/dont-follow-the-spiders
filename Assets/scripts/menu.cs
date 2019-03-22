8b592ea6c4f030ef1ce41712dd8cab9f610be85b
Jaeson Booker <jaesonbooker@gmail.com>
2019-03-21 20:04:10 -0700

    added other menus, quit, and warnings

--- Assets/scripts/menu.cs
+++ Assets/scripts/menu.cs
@@ -8,7 +8,31 @@ public class menu : MonoBehaviour
     // Start is called before the first frame update
     public void battleMode()
     {
+        SceneManager.LoadScene("battleMenu");
+    }
+    public void friendlyMode()
+    {
+        SceneManager.LoadScene("menu2");
+    }
+    public void warning()
+    {
+        SceneManager.LoadScene("warning");
+    }
+    public void forest()
+    {
+        SceneManager.LoadScene("peace");
+    }
+    public void cave()
+    {
+        SceneManager.LoadScene("scene");
+    }
+    public void battleCave()
+    {
         SceneManager.LoadScene("battle");
+    }
+    public void back()
+    {
+        SceneManager.LoadScene("menu");
     }
     public void endGame()
     {
