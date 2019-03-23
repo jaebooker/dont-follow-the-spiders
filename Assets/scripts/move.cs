b2174552412a4208e99d06c3c10a9112952a603d
Jaeson Booker <jaesonbooker@gmail.com>
2019-03-22 19:56:43 -0700

    spider movements and stuff

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 2f;
+    public Rigidbody rb;
+    void Start()
+    {
+        rb.velocity = transform.forward * -speed/4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.forward * -speed/4;
+    }
+}
