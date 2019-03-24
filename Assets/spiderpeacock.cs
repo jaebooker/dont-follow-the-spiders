6cf7cf922a9226269090c27bc0a8cb0827d11028
Jaeson Booker <jaesonbooker@gmail.com>
2019-03-23 19:55:22 -0700

    added stuff and things

---
+++ Assets/spiderpeacock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spiderpeacock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float peacocke;
+    public Animator peacock;
+    void Start()
+    {
+        peacocke = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        peacock.SetFloat("coconut", peacocke);
+    }
+}
