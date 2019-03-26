d60499c958503385e762c7c00e1691cad1b2b69f
Jaeson Booker <jaesonbooker@gmail.com>
2019-03-26 15:17:03 -0700

    fixed a few things

---
+++ Assets/exit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class exit : MonoBehaviour
+{
+    void OnCollisionEnter(Collision spiderInfo)
+    {
+        SceneManager.LoadScene("menu");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
